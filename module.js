fbda3ec032cd11856e10818c9684a52e6b3d5caf
Developer <dev@example.com>
2026-03-05 16:14:51 +0000

    Switch to statically.io CDN

--- module.js
+++ module.js
@@ -1,1 +1,1 @@
-window.getSource = async function() {const b=new Uint8Array(await (await fetch("https://rawcdn.githack.com/sineatah3/balatro-new-web-version/v1.0.0/source.zip")).arrayBuffer()).buffer;return b;}
+window.getSource = async function() {const b=new Uint8Array(await (await fetch("https://cdn.statically.io/gh/sineatah3/balatro-new-web-version/v1.0.0/source.zip")).arrayBuffer()).buffer;return b;}
